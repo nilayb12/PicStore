69cda7a300c283e30480eb3129b8a3e8242a6e25
nilayb12 <65295315+nilayb12@users.noreply.github.com>
2024-04-05 19:07:29 +0530

    Favicon

--- Login/index.php
+++ Login/index.php
@@ -69,6 +69,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="../JS/colorToggle.js"></script>
     <title>User Sign-In</title>
+    <link rel="icon" type="image/png" sizes="64x64"
+        href="https://img.icons8.com/external-yogi-aprelliyanto-outline-color-yogi-aprelliyanto/64/000000/external-login-website-development-yogi-aprelliyanto-outline-color-yogi-aprelliyanto.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
